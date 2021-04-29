882658ff4be3eb4e7d913e692ed5011b2e91d9c7
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2021-04-30 05:17:36 +0600

    tests added for filters

--- tests/Laravel/SnapshotTest.php
+++ tests/Laravel/SnapshotTest.php
@@ -25,4 +25,36 @@ final class SnapshotTest extends TestCase
 
         $this->assertMatchesSnapshot(Artisan::output());
     }
+
+    /** @test */
+    public function filter_by_method(): void
+    {
+        Artisan::call('route:pretty --method=POST');
+
+        $this->assertMatchesSnapshot(Artisan::output());
+    }
+
+    /** @test */
+    public function filter_by_except_path(): void
+    {
+        Artisan::call('route:pretty --except-path=test');
+
+        $this->assertMatchesSnapshot(Artisan::output());
+
+        Artisan::call('route:pretty --except-path=test,some');
+
+        $this->assertMatchesSnapshot(Artisan::output());
+    }
+
+    /** @test */
+    public function filter_by_only_path(): void
+    {
+        Artisan::call('route:pretty --only-path=admin');
+
+        $this->assertMatchesSnapshot(Artisan::output());
+
+        Artisan::call('route:pretty --only-path=admin,some');
+
+        $this->assertMatchesSnapshot(Artisan::output());
+    }
 }
